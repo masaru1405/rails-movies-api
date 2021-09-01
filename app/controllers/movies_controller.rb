d36234513ea0e040b5a6c5f4c2886859fe724dd5
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2021-09-01 20:21:30 +0000

    criado funcao search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,7 @@ class MoviesController < ApplicationController
   before_action :service_movies
   def index
     @movies = @movie_api.search_movie('comics', 2)
+    @search_multi = @movie_api.search_multi('mary',2)
   end
 
   private
--- app/services/movie_api.rb
+++ app/services/movie_api.rb
@@ -5,8 +5,12 @@ class MovieApi
    API_KEY = 'e2e6c0526e3737f2381684d2fd63d354'
    base_uri 'https://api.themoviedb.org/3'
 
-   def search_movie(query, page, language='pt-BR')
-      self.class.get('/search/movie?api_key=' + API_KEY + '&language=' + language + '&query=' + query + '&page= ' + page.to_s)
+   def search_movie(query, page=1, language='pt-BR')
+      self.class.get('/search/movie?api_key=' + API_KEY + '&query=' + query + '&language=' + language + '&page= ' + page.to_s)
+   end
+
+   def search_multi(query, page=1, language='pt-BR')
+      self.class.get('/search/multi?api_key=' + API_KEY + '&query=' + query + '&language=' + language + '&page=' + page.to_s)
    end
 
 end
